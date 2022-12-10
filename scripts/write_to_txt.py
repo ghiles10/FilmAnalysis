56c143b295baf33d2f7a5d507f7ee65a209fe1ef
ghiles idris <96752546+ghiles10@users.noreply.github.com>
2022-12-10 09:56:26 +0000

    first verion for  docker

--- scripts/write_to_txt.py
+++ scripts/write_to_txt.py
@@ -1,12 +1,12 @@
 import film
 
-# écrire vers un fichier txt
-raw_data = film.get_donnees_film()
-
-# écrire les donnes vers le fichier texte 
 def extract_data() : 
 
-    with open('data_film.txt', 'a') as f :
+    """stocke les donnes dans un fichier texte"""
+    
+    # écrire vers un fichier txt
+    raw_data = film.get_donnees_film()
+    with open(r'data_film.txt', 'a') as f :
         for titre, info in raw_data.items() : 
 
             f.write( str(titre)+ '\t' + str(info[0][0])+ '\t' + str(info[0][1]) + '\t' + str(info[0][2])\
@@ -16,4 +16,21 @@ def extract_data() :
 
     print('extracting data done')
 
-extract_data()
+
+
+def write_titre_txt() :
+
+    """ permet d'ecrire le titre des films dans un fichier txt et utilisation dans bash"""
+    
+    raw_data = film.get_donnees_film()
+    with open(r'titre_film.txt', 'a') as f :
+        for titre in raw_data.keys() :
+            f.write(titre + '\n') 
+    f.close()
+
+
+if __name__ == '__main__' : 
+    extract_data()
+    write_titre_txt() 
+
+
