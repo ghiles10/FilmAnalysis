e88548c2dfd272e0dcc5edb1d354828ccc67178e
ghiles idris <96752546+ghiles10@users.noreply.github.com>
2023-01-17 01:54:09 +0100

    Delete notebook directory

--- notebook/install.sh
+++
@@ -1,4 +0,0 @@
-# construction de l'image 
-echo '----> construction a partir du docker file '
-sudo docker build -t projet-linux:1.0 -f ./ .
-
--- notebook/launch.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# lancement 
-sudo docker run -p 5000:5000 projet-linux:1.0 
